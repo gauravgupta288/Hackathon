465a89c4d20c56d6ebb88d74081edba30923a66f
guptagaurav288 <guptagaurav288@gmail.com>
2022-12-12 23:38:13 +0530

    implementing put request

--- src/main/java/com/hackathon/hackathon/dao/SkillsRepository.java
+++ src/main/java/com/hackathon/hackathon/dao/SkillsRepository.java
@@ -2,9 +2,13 @@ package com.hackathon.hackathon.dao;
 
 import com.hackathon.hackathon.entities.Skills;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 public interface SkillsRepository extends JpaRepository<Skills, Integer> {
 
+    @Query("UPDATE skills SET skill = : sn WHERE id = : id")
+    public Skills getUserSkill(@Param("id") int id, @Param("sn") String skillName);
 }
