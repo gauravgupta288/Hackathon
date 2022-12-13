b0c0e05c7bbbf683d1b5d1bb0a6c5d28f59d84ff
guptagaurav288 <guptagaurav288@gmail.com>
2022-12-14 00:05:19 +0530

    unit test for addSkills method

--- src/main/java/com/hackathon/hackathon/entities/Skills.java
+++ src/main/java/com/hackathon/hackathon/entities/Skills.java
@@ -2,7 +2,6 @@ package com.hackathon.hackathon.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 
 @Entity
 @AllArgsConstructor
